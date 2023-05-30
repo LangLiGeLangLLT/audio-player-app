47c17c8694da79e760dbac1d723b04b4ae9a5538
llt <418797644@qq.com>
2023-05-31 01:18:25 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import AudioPlayerV2 from '@/components/AudioPlayerV2'
 export default function Home() {
   return (
     <main className="min-h-screen overflow-x-hidden overflow-y-auto flex flex-col justify-center items-center gap-8 bg-purple-100">
-      {/* <AudioPlayer src="/musics/流星P (minato) _ 初音未来 (初音ミク) - 胧月 (朦胧月色).ogg" /> */}
-      <AudioPlayerV2 src="/musics/流星P (minato) _ 初音未来 (初音ミク) - 胧月 (朦胧月色).ogg" />
+      {/* <AudioPlayer src="/audioPlayer/musics/流星P (minato) _ 初音未来 (初音ミク) - 胧月 (朦胧月色).ogg" /> */}
+      <AudioPlayerV2 src="/audioPlayer/musics/流星P (minato) _ 初音未来 (初音ミク) - 胧月 (朦胧月色).ogg" />
     </main>
   )
 }
